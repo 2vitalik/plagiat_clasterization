a981c1193a76ca7e78f2ea7e1a919e0620b8acf2
vitalik <2vitalik@gmail.com>
2013-08-19 23:57:40 +0300

    reorder functions

--- libs/file.py
+++ libs/file.py
@@ -1,12 +1,12 @@
 
 
+def read_lines(filename):
+    return open(filename).read().split('\n')
+
+
 def file_save(filename, content, encode=None):
     f = open(filename, "w")
     if encode:
         content = content.encode(encode)
     f.write(content)
     f.close()
-
-
-def get_lines(filename):
-    return open(filename).read().split('\n')
