71a5bf8ab9be54969f3f5d7f260d6fcf83d13a3d
vitalik <2vitalik@gmail.com>
2013-08-21 15:46:16 +0300

    add ".mystem/mystem.exe"

--- libs/mystem.py
+++ libs/mystem.py
@@ -3,7 +3,7 @@ from libs.tools import w2u
 
 
 def mystem(text):
-    process = Popen('".data/mystem.exe" -cnig', shell=False, cwd='.',
+    process = Popen('".mystem/mystem.exe" -cnig', shell=False, cwd='.',
                     stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = process.communicate(text.encode('cp1251'))
     return w2u(out)
