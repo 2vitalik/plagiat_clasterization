b166c72710a19932d245052b9285a2f599bdf91b
Vitalik <2vitalik@gmail.com>
2013-09-15 20:28:44 +0300

    steps template, class

--- main/steps.py
+++ main/steps.py
@@ -2,26 +2,23 @@ from main.models import News, NewsContent, NewsParagraph
 from plagiat_clasterization import settings
 
 
-def load_news_from_folder():
-    """ create News and NewsContent """
-    News.objects.load_from_folder(settings.NEWS_PATH)
+class Steps(object):
+    def load_news_from_folder(self):
+        """ create News and NewsContent """
+        News.objects.load_from_folder(settings.NEWS_PATH)
 
+    def create_paragraphs(self):
+        """ create NewsParagraph """
+        NewsContent.objects.create_paragraphs()
 
-def create_paragraphs():
-    """ create NewsParagraph """
-    NewsContent.objects.create_paragraphs()
+    def create_title_stemmed(self):
+        """ create TitleStemmed """
+        News.objects.create_stems()
 
+    def create_news_stemmed(self):
+        """ create NewsStemmed """
+        NewsContent.objects.create_stems()
 
-def create_title_stemmed():
-    """ create TitleStemmed """
-    News.objects.create_stems()
-
-
-def create_news_stemmed():
-    """ create NewsStemmed """
-    NewsContent.objects.create_stems()
-
-
-def create_paragraph_stemmed():
-    """ create ParagraphStemmed """
-    NewsParagraph.objects.create_stems()
+    def create_paragraph_stemmed(self):
+        """ create ParagraphStemmed """
+        NewsParagraph.objects.create_stems()
--- main/views.py
+++ main/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import TemplateView
+from main.steps import load_news_from_folder, create_paragraphs
 
 
 class MainView(TemplateView):
     template_name = 'main.html'
 
-
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        steps = {
+            load_news_from_folder: "create News and NewsContent",
+            create_paragraphs: "create NewsParagraph",
+        }
+        context.update({
+            'steps': steps
+        })
+        return context
