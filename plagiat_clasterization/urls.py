d58113fcd5b43a8bdbde93cc09ecf1c7cb6a3c14
Vitalik <2vitalik@gmail.com>
2013-09-15 20:09:35 +0300

    empty main page

--- plagiat_clasterization/urls.py
+++ plagiat_clasterization/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from main.views import MainView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', MainView.as_view(), name='main')
 )
